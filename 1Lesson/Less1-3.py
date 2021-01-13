32fdb0bbaaf50f665a5ebbea272f3a87fe842cc3
pitdim <77243843+pitdim@users.noreply.github.com>
2021-01-14 00:27:02 +0500

    Добавил декоратор

--- 1Lesson/Less1-3.py
+++ 1Lesson/Less1-3.py
@@ -1,23 +1,21 @@
 from time import time
 
+def DecoratoTime(func):
+    def wrapper(arg):
+        Start_Time = time()
+        Stap = func(arg)
+        Finish_Time = time()
+        print("Выполнялось в секундах", Finish_Time - Start_Time)
+        return Stap
+    return wrapper
 
+
+@DecoratoTime
 def GoToNewStep(a):
     b = 0
     for i in range(0, 10000):
         b += a ** i
-
     return b
-
-
-
-def TimeStartToEnd():
-    Start_Time = time()
-    Stap = GoToNewStep(199)
-    Finish_Time = time()
-    print(Finish_Time - Start_Time)
-
-
-
 
 
 n = GoToNewStep(199)
