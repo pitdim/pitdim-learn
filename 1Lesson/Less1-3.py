8892b72590a864ac2766130d15772a59e659d9f0
pitdim <77243843+pitdim@users.noreply.github.com>
2021-01-13 23:08:29 +0500

    До декоратора

--- 1Lesson/Less1-3.py
+++ 1Lesson/Less1-3.py
@@ -1,3 +1,6 @@
+from time import time
+
+
 def GoToNewStep(a):
     b = 0
     for i in range(0, 10000):
@@ -5,5 +8,20 @@ def GoToNewStep(a):
 
     return b
 
+
+
+def TimeStartToEnd():
+    Start_Time = time()
+    Stap = GoToNewStep(199)
+    Finish_Time = time()
+    print(Finish_Time - Start_Time)
+
+
+
+
+
 n = GoToNewStep(199)
 print(n)
+
+
+
