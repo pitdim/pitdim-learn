be7f2eb0fa927423c2957b55b7a51f0f14265a11
pitdim <77243843+pitdim@users.noreply.github.com>
2021-01-21 23:35:59 +0500

    MyIzmk

--- MyL1.py
+++ MyL1.py
@@ -1,17 +1,22 @@
 class K1:
+
     def __init__(self):
         self.a = 1
         self.b = 500
-    def __eq__(self, other):
-        return (self.a == other.a)
 
-    def _
+    def __eq__(self, other):
+        return self.a == other.a
+
+
 class K2:
+
     def __init__(self):
         self.a = 1
         self.b = 100
+
     def __eq__(self, other):
-        return (self.a == other.a)
+        return self.a == other.a
+
 
 ObK1 = K1()
 ObK2 = K2()
