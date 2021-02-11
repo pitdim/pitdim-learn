a7b9311bd1350e84d75752a3a3acd5db89b13123
pitdim <77243843+pitdim@users.noreply.github.com>
2021-02-11 23:03:15 +0500

    Окно приветствия

--- 1Lesson/ClassHomework/main.py
+++ 1Lesson/ClassHomework/main.py
@@ -1,9 +1,11 @@
+from dataclasses import dataclass
 import configparser
 from tkinter import *
 import csv
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
+from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm.exc import NoResultFound
 step = 1
 mass = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
@@ -59,43 +61,93 @@ def change_name(event):
 
 window = Tk()
 window.title("Window")
-window.geometry("800x600")
+window.geometry("500x500")
 
 my_config = configparser.ConfigParser()
 my_config.read("config.ini")
 
-
-
-
-try:
-    name_games = my_config["main"]["name"]
-
-    label = Label(window, text="Приветствую! " + name_games, font="Tahoma 19")
-    label.place(x=100, y=0, width=800, height=33)
-except KeyError:
-    label = Label(window, text="Представься:", font="Tahoma 19")
-    label.pack()
-
-    text2 = Entry(window, font="Tahoma 20")
-    text2.place(x=300, y=50, width=800, height=33)
-    text2.bind('<Return>', change_name)
-
 tt = create_engine("sqlite:///game_base.db")
 base = declarative_base(bind=tt)
+
 ses = sessionmaker(bind=tt)
 Session = scoped_session(ses)
 
+meta = MetaData()
+
+students = Table(
+    'player', meta,
+    Column('id', Integer, primary_key=True),
+    Column('playername', String, unique=True),
+    Column('wines', Integer, default=0),
+)
+
+meta.create_all(tt)
+
+
 class Player(base):
     __tablename__ = "player"
-
     id = Column(Integer, primary_key=True)
     playername = Column(String(32), unique=True)
-    wines = Column(Integer)
+    wines = Column(Integer, default=0)
+
+    def __str__(self):
+        return f"Игрок - {self.playername}, счет у этого игрока - {self.wines}"
+
+    def __repr__(self):
+        return str(self)
 
 
-user = Player(playername="Kolya")
+def creat_new_user(name):
+    player_data = Player(playername=name)
+    session.add(player_data)
+    session.commit()
+    return player_data
+
+
+@dataclass
+class Player_game:
+    base_player: Player
+    symbol: str
+
+
+
+symbol_player = "x"
+label = Label(window, text=f"Представься {symbol_player}:", font="Tahoma 19")
+text2 = Entry(window, font="Tahoma 20")
+text2.bind('<Return>', change_name)
+
+label2 = Label(window, text="", font="Tahoma 19")
+label3 = Label(window, text="", font="Tahoma 19")
+
+
+def show_hello():
+    label.place(x=0, y=10, width=200, height=33)
+    text2.place(x=200, y=10, width=200, height=33)
+    label2.place(x=0, y=40, width=200, height=33)
+    label3.place(x=0, y=70, width=200, height=33)
+
+def hide_hello():
+    label.place_forget()
+    text2.place_forget()
+    label2.place_forget()
+    label3.place_forget()
+
+
+show_hello()
+
+my_name = "Ольга"
+
 session = Session()
-session.add(user)
+try:
+    user_bd = session.query(Player).filter_by(playername=my_name).one()
+except NoResultFound:
+    user_bd = creat_new_user(my_name)
+
+user_bd.wines = 2
+print(user_bd.wines)
+print(user_bd.playername)
+session.commit()
+
 session.close()
 
 with open("game_save.csv") as f:
@@ -111,32 +163,40 @@ def returne_mass_count(x, y):
     else:
      return ""
 
+
 button1 = Button(window, text=returne_mass_count(0, 0), command=lambda: PutButton(button1, 0, 0), font="Tahoma 60")
-button1.place(x=0, y=0, width=100, height=100)
-
 button2 = Button(window, text=returne_mass_count(0, 1), command=lambda: PutButton(button2, 0, 1), font="Tahoma 60")
-button2.place(x=100, y=0, width=100, height=100)
-
 button3 = Button(window, text=returne_mass_count(0, 2), command=lambda: PutButton(button3, 0, 2), font="Tahoma 60")
-button3.place(x=200, y=0, width=100, height=100)
-
 button4 = Button(window, text=returne_mass_count(1, 0), command=lambda: PutButton(button4, 1, 0), font="Tahoma 60")
-button4.place(x=0, y=100, width=100, height=100)
-
 button5 = Button(window, text=returne_mass_count(1, 1), command=lambda: PutButton(button5, 1, 1), font="Tahoma 60")
-button5.place(x=100, y=100, width=100, height=100)
-
 button6 = Button(window, text=returne_mass_count(1, 2), command=lambda: PutButton(button6, 1, 2), font="Tahoma 60")
-button6.place(x=200, y=100, width=100, height=100)
-
 button7 = Button(window, text=returne_mass_count(2, 0), command=lambda: PutButton(button7, 2, 0), font="Tahoma 60")
-button7.place(x=0, y=200, width=100, height=100)
-
 button8 = Button(window, text=returne_mass_count(2, 1), command=lambda: PutButton(button8, 2, 1), font="Tahoma 60")
-button8.place(x=100, y=200, width=100, height=100)
-
 button9 = Button(window, text=returne_mass_count(2, 2), command=lambda: PutButton(button9, 2, 2), font="Tahoma 60")
-button9.place(x=200, y=200, width=100, height=100)
+
+
+
+def view_button():
+    button1.place(x=0, y=0, width=100, height=100)
+    button2.place(x=100, y=0, width=100, height=100)
+    button3.place(x=200, y=0, width=100, height=100)
+    button4.place(x=0, y=100, width=100, height=100)
+    button5.place(x=100, y=100, width=100, height=100)
+    button6.place(x=200, y=100, width=100, height=100)
+    button7.place(x=0, y=200, width=100, height=100)
+    button8.place(x=100, y=200, width=100, height=100)
+    button9.place(x=200, y=200, width=100, height=100)
+
+def hide_button():
+     button1.place_forget()
+     button2.place_forget()
+     button3.place_forget()
+     button4.place_forget()
+     button5.place_forget()
+     button6.place_forget()
+     button7.place_forget()
+     button8.place_forget()
+     button9.place_forget()
 
 
 window.mainloop()
